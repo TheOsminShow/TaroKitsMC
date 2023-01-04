e148e2d85435f262b3d35ed4f4bd2da54499b8f8
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:13:22 +0100

    Update PlayerDataManager.java

--- src/com/dnInventorySaver/plugin/Manager/PlayerData/PlayerDataManager.java
+++ src/com/dnInventorySaver/plugin/Manager/PlayerData/PlayerDataManager.java
@@ -86,9 +86,3 @@
 /* 86 */     return PermissionUtil.playerHasPermissions(this.player, paramString);
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\PlayerData\PlayerDataManager.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
