56ca3b339116128a7c1aad5de6397cd67e2a1e9a
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:14:00 +0100

    Update DeleteUtil.java

--- src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/DeleteUtil.java
+++ src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/DeleteUtil.java
@@ -97,9 +97,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\PlayerData\Util\DeleteUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
