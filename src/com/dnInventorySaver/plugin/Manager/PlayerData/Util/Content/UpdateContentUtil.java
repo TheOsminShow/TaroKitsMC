2437d7865bab89fdbd5190322bccbaa280dcfbca
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:17:24 +0100

    Update UpdateContentUtil.java

--- src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/Content/UpdateContentUtil.java
+++ src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/Content/UpdateContentUtil.java
@@ -60,9 +60,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\PlayerData\Util\Content\UpdateContentUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
