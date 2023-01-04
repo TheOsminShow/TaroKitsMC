9661d10bd72678c1f3abdb0e7b9256dc09c30ad0
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:06:22 +0100

    Update ArmorUtil.java

--- src/com/dnInventorySaver/plugin/Util/ArmorUtil.java
+++ src/com/dnInventorySaver/plugin/Util/ArmorUtil.java
@@ -25,9 +25,3 @@
 /* 25 */     return paramItemStack.getType().name().endsWith("_BOOTS");
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Util\ArmorUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
