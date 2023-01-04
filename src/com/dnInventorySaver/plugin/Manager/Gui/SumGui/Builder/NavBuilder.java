9ef87ba05e993debfcc3e42d688c743c8c066f0e
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:13:04 +0100

    Update NavBuilder.java

--- src/com/dnInventorySaver/plugin/Manager/Gui/SumGui/Builder/NavBuilder.java
+++ src/com/dnInventorySaver/plugin/Manager/Gui/SumGui/Builder/NavBuilder.java
@@ -57,9 +57,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\Gui\SumGui\Builder\NavBuilder.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
