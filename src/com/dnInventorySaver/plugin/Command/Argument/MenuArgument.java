da3f4ac9c92a8b327a0d34e8327c880b7260ab59
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:04:49 +0100

    Update MenuArgument.java

--- src/com/dnInventorySaver/plugin/Command/Argument/MenuArgument.java
+++ src/com/dnInventorySaver/plugin/Command/Argument/MenuArgument.java
@@ -45,9 +45,3 @@
 /*    */         });
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Command\Argument\MenuArgument.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
