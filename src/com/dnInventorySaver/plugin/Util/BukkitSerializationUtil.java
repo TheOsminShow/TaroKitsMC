bb6677de97cbf1b740f757f679c1f73617b1d647
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:06:32 +0100

    Update BukkitSerializationUtil.java

--- src/com/dnInventorySaver/plugin/Util/BukkitSerializationUtil.java
+++ src/com/dnInventorySaver/plugin/Util/BukkitSerializationUtil.java
@@ -47,9 +47,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Util\BukkitSerializationUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
