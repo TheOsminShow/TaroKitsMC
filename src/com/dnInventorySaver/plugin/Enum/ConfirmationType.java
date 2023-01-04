0eac485e60d9abc64fb30ce8f049275b9af0a3da
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:18:50 +0100

    Update ConfirmationType.java

--- src/com/dnInventorySaver/plugin/Enum/ConfirmationType.java
+++ src/com/dnInventorySaver/plugin/Enum/ConfirmationType.java
@@ -6,9 +6,3 @@
 /* 6 */   CLEAR,
 /* 7 */   LOAD;
 /*   */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Enum\ConfirmationType.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
