17c1a23d5c62cd1ff4410680d9c25f3ef1c02b51
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:08:23 +0100

    Update RankInfoUtil.java

--- src/com/dnInventorySaver/plugin/Util/RankInfoUtil.java
+++ src/com/dnInventorySaver/plugin/Util/RankInfoUtil.java
@@ -20,9 +20,3 @@
 /* 20 */     return Core.getConfigManager().getInt("PERMISSIONS." + paramString + ".save-price");
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Util\RankInfoUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
