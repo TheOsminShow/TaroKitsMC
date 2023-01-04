f0fae68af3aad99b5ed8a1e54212d5ade4c80cc0
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:07:02 +0100

    Update ClickableMessageUtil.java

--- src/com/dnInventorySaver/plugin/Util/ClickableMessageUtil.java
+++ src/com/dnInventorySaver/plugin/Util/ClickableMessageUtil.java
@@ -17,9 +17,3 @@
 /* 17 */     paramPlayer.spigot().sendMessage((BaseComponent)textComponent);
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Util\ClickableMessageUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
