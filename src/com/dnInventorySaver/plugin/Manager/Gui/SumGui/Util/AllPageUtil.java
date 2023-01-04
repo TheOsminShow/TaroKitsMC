3b31999452f6f18b052544248be011f2d5516f30
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:12:14 +0100

    Update AllPageUtil.java

--- src/com/dnInventorySaver/plugin/Manager/Gui/SumGui/Util/AllPageUtil.java
+++ src/com/dnInventorySaver/plugin/Manager/Gui/SumGui/Util/AllPageUtil.java
@@ -16,9 +16,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\Gui\SumGui\Util\AllPageUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
