46f02ca18b533d70b8db8abb7b996be8dad168cb
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:05:12 +0100

    Update SaveArgument.java

--- src/com/dnInventorySaver/plugin/Command/Argument/SaveArgument.java
+++ src/com/dnInventorySaver/plugin/Command/Argument/SaveArgument.java
@@ -124,9 +124,3 @@
 /*    */         });
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Command\Argument\SaveArgument.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
