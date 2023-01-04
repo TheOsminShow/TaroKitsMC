8ef4c232816f8c2f6f418a942e4112d230390e4b
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:09:37 +0100

    Update FlatfileManager.java

--- src/com/dnInventorySaver/plugin/Manager/Database/Flatfile/FlatfileManager.java
+++ src/com/dnInventorySaver/plugin/Manager/Database/Flatfile/FlatfileManager.java
@@ -18,9 +18,3 @@
 /* 18 */     if (!playerDataFolder.exists()) playerDataFolder.mkdirs(); 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\Database\Flatfile\FlatfileManager.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
