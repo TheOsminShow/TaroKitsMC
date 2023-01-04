ed38951b71f75b04b49c165ed473bd7e2843f1f7
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:05:25 +0100

    Update SyncArgument.java

--- src/com/dnInventorySaver/plugin/Command/Argument/SyncArgument.java
+++ src/com/dnInventorySaver/plugin/Command/Argument/SyncArgument.java
@@ -79,9 +79,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Command\Argument\SyncArgument.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
