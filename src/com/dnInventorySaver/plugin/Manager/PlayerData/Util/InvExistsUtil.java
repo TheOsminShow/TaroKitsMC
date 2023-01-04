8c8cdcc2c7af6c414dc6107fb287f7e0ca87a99a
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:14:25 +0100

    Update InvExistsUtil.java

--- src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/InvExistsUtil.java
+++ src/com/dnInventorySaver/plugin/Manager/PlayerData/Util/InvExistsUtil.java
@@ -16,9 +16,3 @@
 /* 16 */     return false;
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\PlayerData\Util\InvExistsUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
