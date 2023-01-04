8bd6d9f5a3bf66da9ee691af40cb0f12458a9b31
SMarc1 <34447179+SMarc1@users.noreply.github.com>
2023-01-05 00:11:14 +0100

    Update DeleteUtil.java

--- src/com/dnInventorySaver/plugin/Manager/Gui/InventoryGui/Util/DeleteUtil.java
+++ src/com/dnInventorySaver/plugin/Manager/Gui/InventoryGui/Util/DeleteUtil.java
@@ -75,9 +75,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              C:\Users\marcp\Downloads\InventorySaver.jar!\com\dnInventorySaver\plugin\Manager\Gui\InventoryGui\Util\DeleteUtil.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
